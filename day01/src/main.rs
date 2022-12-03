137ad2918eec46c97635145fbd47e495beb68a5c
M <mchaker@users.noreply.github.com>
2022-12-03 18:38:04 +0000

    clean up proj dirs

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 
 fn main() {
-	let contents = fs::read_to_string("./input-01.txt")
+	let contents = fs::read_to_string("input-01.txt")
         .expect("Should have been able to read the input file");
 
 	println!("{contents}");
