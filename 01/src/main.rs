dec8c1ec574b558ecf34ddf8d41ada86d4f1cc4d
M <mchaker@users.noreply.github.com>
2022-12-28 06:16:46 +0000

    i will get at least one problem done this year

--- 01/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-use std::fs;
-
-fn main() {
-	let contents = fs::read_to_string("./input-01.txt")
-        .expect("Should have been able to read the input file");
-
-	println!("{contents}");
-}
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
+//use std::env;
 use std::fs;
 
 fn main() {
 	let contents = fs::read_to_string("input-01.txt")
-        .expect("Should have been able to read the input file");
+		.expect("Should have been able to read the input file");
+
+	// take in each group of lines and add them to a vector
 
 	println!("{contents}");
+	println!("test");
 }
